e3d0683e258c3ab24c54c727bc8a111eb57fdcd8
knoebber <knoebber@gmail.com>
2017-02-22 13:22:49 -0600

    fix test

--- test_parse.sh
+++ test_parse.sh
@@ -1,8 +1,1 @@
-echo POSTING
-curl -X POST \
-  -H "X-Parse-Application-Id: f8dd0d83bdc78b82378bb69e725d2f28" \
-  -H "Content-Type: application/json" \
-  -d '{"name":"testevent","date":"tomorrow","isitlit":true}' \
-  http://localhost:8999/parse/classes/Events
-
-curl -H "X-Parse-Application-Id: f8dd0d83bdc78b82378bb69e725d2f28" http://localhost:8999/parse/classes/Events
+curl -X GET -H "X-Parse-Application-Id: f8dd0d83bdc78b82378bb69e725d2f28" http://appdev.grinnell.edu/api/events/v1/classes/Event
