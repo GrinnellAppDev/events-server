f47f98a27d5678e8faa74e6edf490470c007bc82
knoebber <knoebber@gmail.com>
2017-03-14 18:46:00 -0500

    add bash

--- test_parse.sh
+++ test_parse.sh
@@ -1,1 +1,2 @@
-curl -X GET -H "X-Parse-Application-Id: f8dd0d83bdc78b82378bb69e725d2f28" http://appdev.grinnell.edu/api/events/v1/classes/Event
+#!/bin/bash
+curl -X GET -H "X-Parse-Application-Id: f8dd0d83bdc78b82378bb69e725d2f28" https://appdev.grinnell.edu/api/events/v1/classes/Event
