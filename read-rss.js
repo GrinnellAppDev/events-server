fb26ca50febfe97baf5fe453690bdebf4590bf08
knoebber <knoebber@gmail.com>
2017-03-17 13:50:58 -0500

    add parsing for start time, end time, and optional description

--- read-rss.js
+++ read-rss.js
@@ -9,6 +9,43 @@ var EventObject = Parse.Object.extend("Event");
 Parse.initialize("f8dd0d83bdc78b82378bb69e725d2f28");
 Parse.serverURL = "http://localhost:8999/parse/";
 
+/* converts Thursday, March 16, 2017, 11am&nbsp;&ndash;&nbsp;12pm"
+   to "Thursday, March 16, 2017, 11:00 pm" 
+*/
+
+function convertDate(date){
+  var split = date.split(";");
+  var endTime = split[split.length-1];
+  var startDate = date.split("&")[0];
+  split = startDate.split(",");
+  var startTime = split[split.length-1]; 
+  var day = split[0] + ","+ split[1] + "," + split[2]; //Thursday, March 16, 2015
+  startDate = day + " " + convertTime(startTime); 
+  var endDate = day + " " + convertTime(endTime); 
+  return [new Date(startDate),new Date(endDate)];
+}
+/* helper for convertDate */
+function convertTime(time){
+  time = time.replace(" ",""); //removes leading space
+  if (time.length <= 4) {
+    var ar = time.split('');
+    ar.splice(ar.length - 2,0,":00");
+    time = ar.join('');
+  }
+  var ar = time.split('');
+  ar.splice(ar.length - 2,0," ");
+  return ar.join(''); 
+}
+
+/*searchs list of <br> items for <p> tag
+  returns null if not found
+*/
+function findDescription(description){
+  var pattern = /\<\/*p\>/ig
+  var d = description.filter((element) => pattern.test(element));
+  return d.length == 0 ? null : d[0].replace(pattern,"");
+} 
+
 req.on('response', function (res) {
   var stream = this; // `this` is `req`, which is a stream
   if (res.statusCode !== 200) {
@@ -19,34 +56,43 @@ req.on('response', function (res) {
   }
 });
 
-//var test;
 feedparser.on('readable', function () {
   // This is where the action is!
   var stream = this; // `this` is `feedparser`, which is a stream
-  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  var meta = this.meta; // **NOTE** the "meta" is always available in the conteeventt of the feedparser instance
   var item;
-  var e;
   var description;
+  var evnt;  
   while (item = stream.read()) {
+    evnt = new EventObject();
     description = item.description.split("<br/>"); //description is an html element from the RSS feed
-    e = new EventObject(); 
-    e.set("eventid", item.guid);
-    e.set("title", item.title);
-    e.set("location",description[0]); //if the RSS feed format changes, these will need to be changed
-    e.set("date", description[1]);
-    events.push(e);
-    
-   // test = item;
+    times = convertDate(description[1]);
+    evnt.set("eventid", item.guid);
+    evnt.set("title", item.title);
+    evnt.set("description",findDescription(description));
+    evnt.set("location",description[0]); //if the RSS feed format changes, these will need to be changed
+    evnt.set("startTime",times[0]);
+    evnt.set("endTime",times[1]);
+    events.push(evnt);
   }
 });
 
 feedparser.on('finish', function () {
+  //console.log("events updated " + updateCount);
+  //console.log("total events " + totalCount);
+  //console.log("new events " + (totalCount-updateCount));
+  //var query = new Parse.Query(EventObject).contains("eventid",item.guid);
+  //var log = query.find({success:function(results) {
   //var d = test.description.split("<br/>");
   //console.log(d);
-  console.log("Saving " + events.length + " events"); 
   //events[0].save();
-  for (e of events)
+  
+  //var query = new Parse.Query(Event);
+  //query.contains("eventid",
+  console.log("saving " + events.length + " events");
+  for (var e of events)
   {
     e.save();
   }
+ 
 });
